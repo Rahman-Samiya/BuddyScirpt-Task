5507f9aad994b894f63eb5b8dce81d8fea8643db
samiyarahman540@gmail.com <samiyarahman540@gmail.com>
2026-04-20 01:07:32 +0600

    Add CommentResource for API comment responses

---
+++ backend/app/Http/Resources/Comment/CommentResource.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Resources\Comment;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class CommentResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'parent_id' => $this->parent_id,
+            'content' => $this->content,
+            'likes_count' => $this->likes_count ?? $this->whenCounted('likes'),
+            'user' => $this->whenLoaded('user', function () {
+                return [
+                    'first_name' => $this->user->first_name,
+                    'last_name' => $this->user->last_name,
+                ];
+            }),
+            'replies' => CommentResource::collection($this->whenLoaded('replies')),
+            'created_at' => $this->created_at?->toDateTimeString(),
+            'updated_at' => $this->updated_at?->toDateTimeString(),
+        ];
+    }
+}
